3c62fa11315d40776403530a3fd5118c205c968f
Shoru <bapcui596@gmail.com>
2023-11-30 21:42:54 +0700

    Add redis timeout

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const redisClient = createClient(
         password: process.env.REDIS_PASSWORD,
         socket: {
             host: process.env.REDIS_HOST,
-            port: process.env.REDIS_PORT
+            port: process.env.REDIS_PORT,
+            connectTimeout: 50000,
         }
     }
 )
