ef54cd267219f65aadc4573f56f23151a894e24f
Shoru <bapcui596@gmail.com>
2023-12-03 00:16:10 +0700

    Employee models change

--- models/employee.js
+++ models/employee.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
-const Gender = require('../enums/gender');
+const { imageSchema } = require('./image');
+const { cloudinary } = require('../cloudinary');
+const User = require('./user');
 const EmployeeSchema = new Schema({
     name: {
         type: String,
@@ -23,10 +24,43 @@ const EmployeeSchema = new Schema({
         type: String,
         required: false,
     },
-    avatar: { //URL
-        type: String,
+    avatar: {
+        type: Schema.Types.Mixed,
+        required: false,
+    },
+    salary: {
+        type: Number,
+        default: 0,
         required: false,
     },
 });
+//Post middleware to delete image from cloudinary when employee is deleted.
+EmployeeSchema.post('findOneAndDelete', async function (doc) {
+    if (doc && doc.avatar && doc.avatar.fileName) {
+        await cloudinary.uploader.destroy(doc.avatar.fileName, function (err, res) {
 
+            if (err) {
+                console.error("Error deleting image from cloudinary: ", err);
+
+            }
+            else {
+                console.log("Image deleted from cloudinary: ", res);
+            }
+        });
+
+        // Find account associated with employee using id
+        const user = await User.findOne({ employeeId: doc._id });
+
+        // Delete account
+        if (user) {
+            await User.findByIdAndDelete(user._id);
+
+        }
+        else
+            console.log('No user found');
+    }
+    else {
+        console.log('No image found');
+    }
+});
 module.exports = mongoose.model('Employee', EmployeeSchema);
