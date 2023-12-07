c6417dba584af43bc65b57f24ea1f15dec40d1a8
Shoru <bapcui596@gmail.com>
2023-12-07 23:08:22 +0700

    refactor: move some pagination funcs for product

--- routes/products.js
+++ routes/products.js
@@ -6,42 +6,47 @@ const { authorize } = require('../middleware/auth');
 const multer = require('multer');
 const { storage } = require('../cloudinary');
 const upload = multer({ storage, limits: { fileSize: 10000000, }, dest: 'product/' }).array('images', 5, errorMaxImages);
-
+const Product = require('../models/product');
+const ValidatePagination = require('../middleware/validatePageLimit');
+async function UploadImages(req, res, next) {
+    try {
+        await upload(req, res, function (err) {
+            if (err instanceof multer.MulterError) {
+                // A Multer error occurred when uploading.
+                console.error("Multer error: ", err);
+                return res.status(500).json({ error: 'Error uploading file.' });
+            }
+            else if (err) {
+                // An unknown error occurred when uploading.
+                console.error("Unknown error: ", err);
+                console.log(req.headers);
+                return res.status(500).json({ error: 'Error uploading file.' });
+            }
+            next();
+        });
+    } catch (error) {
+        // Handle other errors
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error.' });
+    }
+}
 function errorMaxImages(req, res, next) {
     if (req.files.length > 5) {
         return res.status(400).json({ error: 'Maximum 5 images allowed.' });
     }
     next();
 }
-router.route('/')
-    .get(authorize(['employee', 'admin']), productController.getProducts)
-    .post(authorize(['employee', 'admin']), async (req, res) => {
-        try {
-            await upload(req, res, function (err) {
-                if (err instanceof multer.MulterError) {
-                    // A Multer error occurred when uploading.
-                    console.error("Multer error: ", err);
-                    return res.status(500).json({ error: 'Error uploading file.' });
-                } else if (err) {
-                    // An unknown error occurred when uploading.
-                    console.error("Unknown error: ", err);
-                    console.log(req.headers);
-                    return res.status(500).json({ error: 'Error uploading file.' });
-                }
+const ValidatePageWrapper = async (req, res, next) => {
+    ValidatePagination(req, res, next, () => Product.countDocuments());
+}
 
-                // File uploaded successfully, continue with productController.addProduct
-                productController.addProduct(req, res);
-            });
-        } catch (error) {
-            // Handle other errors
-            console.error(error);
-            res.status(500).json({ error: 'Internal server error.' });
-        }
-    });
+router.route('/')
+    .get(authorize(['employee', 'admin']), ValidatePageWrapper, productController.getProducts)
+    .post(authorize(['employee', 'admin']), UploadImages, productController.addProduct);
 
 router.route('/:id')
     .get(authorize(['employee', 'admin']), productController.getProduct)
-    .patch(authorize(['employee', 'admin']), productController.updateProduct)
+    .patch(authorize(['employee', 'admin']), UploadImages, productController.updateProduct)
     .delete(authorize(['employee', 'admin']), productController.deleteProduct);
 
 
