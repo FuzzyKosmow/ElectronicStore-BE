a5e155717149aaf870d94739bb924b1eab71a9f8
Shoru <bapcui596@gmail.com>
2023-12-10 07:49:46 +0700

    feat(customer): type validation

---
+++ middleware/typeValidation/customer.js
@@ -0,0 +1,32 @@
+const joi = require('joi');
+const CreateCustomerSchema = joi.object({
+    name: joi.string().required().max(25),
+    address: joi.string().max(50),
+    phoneNumber: joi.string().max(15),
+});
+
+const UpdateCustomerSchema = joi.object({
+    name: joi.string().max(25),
+    address: joi.string().max(50),
+    phoneNumber: joi.string().max(15),
+});
+
+const ValidateCreateCustomer = (req, res, next) => {
+    const { error } = CreateCustomerSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message, success: false });
+    }
+    next();
+}
+const ValidateUpdateCustomer = (req, res, next) => {
+    const { error } = UpdateCustomerSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message, success: false });
+    }
+    next();
+}
+
+module.exports = {
+    ValidateCreateCustomer,
+    ValidateUpdateCustomer,
+};
--- routes/customers.js
+++ routes/customers.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { authorize } = require('../middleware/auth');
 const customerController = require('../controllers/customers');
-
+const { ValidateCreateCustomer, ValidateUpdateCustomer } = require('../middleware/typeValidation/customer');
 
 router.route('/')
     .get(authorize(['employee', 'admin']), customerController.getCustomers)
-    .post(authorize(['employee', 'admin']), customerController.addCustomer);
+    .post(authorize(['employee', 'admin']), ValidateCreateCustomer, customerController.addCustomer);
 
 router.route('/:id')
     .get(authorize(['employee', 'admin']), customerController.getCustomer)
-    .patch(authorize(['employee', 'admin']), customerController.updateCustomer)
+    .patch(authorize(['employee', 'admin']), ValidateUpdateCustomer, customerController.updateCustomer)
     .delete(authorize(['employee', 'admin']), customerController.deleteCustomer);
 
 module.exports = router;
