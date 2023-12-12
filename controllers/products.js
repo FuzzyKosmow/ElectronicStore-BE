2d34760fa3679bdbabbfe77f3a4037e471cccff4
Shoru <bapcui596@gmail.com>
2023-12-13 01:48:25 +0700

    improvement(product): handling action on quantity Also add more filter similiar to sellPrice Also add documentation

--- controllers/products.js
+++ controllers/products.js
@@ -8,6 +8,9 @@ const { cloudinary } = require('../cloudinary');
 //brand, type, name, manufacturer, countryOrigin, sellPrice, sellPriceMin, sellPriceMax
 //In brand , name, manufacturer, countryOrigin, will use regex to find all products that contain the string
 //In sellPrice, sellPriceMin, sellPriceMax, will find all products that have sellPrice >= sellPriceMin and sellPrice <= sellPriceMax
+//Quantity is similiar to sellPrice
+
+
 function ConvertProductQuery(query) {
     if (!query) {
         return {};
@@ -48,6 +51,20 @@ function ConvertProductQuery(query) {
             filter.sellPrice = { $lte: query.sellPriceMax };
         }
     }
+    if (query.quantity) {
+        filter.quantity = query.quantity;
+    }
+    if (query.quantityMin) {
+        filter.quantity = { $gte: query.quantityMin };
+    }
+    if (query.quantityMax) {
+        if (filter.quantity) {
+            filter.quantity.$lte = query.quantityMax;
+        }
+        else {
+            filter.quantity = { $lte: query.quantityMax };
+        }
+    }
 
     return filter;
 
@@ -68,14 +85,14 @@ module.exports.getProducts = async (req, res) => {
     }
     try {
         results.results = await Product.find(filter).limit(limit).skip(startIndex).exec();
-        res.status(200).json({ ...results, success: true });
+        res.status(200).json({ ...results });
     } catch (error) {
-        res.status(500).json({ error: error, success: false });
+        next(error);
     }
 }
 
 //Accept in multi-part form data. Append images multiple times to add multiple images
-module.exports.addProduct = async (req, res) => {
+module.exports.addProduct = async (req, res, next) => {
     const product = new Product(req.body);
     product.images = [];
     // Check if the file was successfully uploaded
@@ -100,9 +117,9 @@ module.exports.addProduct = async (req, res) => {
     }
     try {
         const savedProduct = await product.save();
-        res.status(200).json({ msg: 'Product added', product: savedProduct, success: true });
+        res.status(200).json({ msg: 'Product added', product: savedProduct });
     } catch (error) {
-        res.status(500).json({ error: error, success: false });
+        next(error);
     }
 }
 //Singular, no filter
@@ -111,30 +128,61 @@ module.exports.getProduct = async (req, res) => {
         const { id } = req.params;
         const product = await Product.findById(id);
         if (!product) {
-            return res.status(404).json({ error: 'Product not found', success: false });
+            return res.status(404).json({ error: 'Product not found' });
         }
         res.json({ product });
     } catch (e) {
         console.error(e);
-        res.status(500).json({ error: 'Internal Server Error', success: false });
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 }
 
-//Update product. imagesDel contains the images that will be deleted (array of image id)
-//files contains the new images that will be added (array of image)
-module.exports.updateProduct = async (req, res) => {
+//Update product: imagesDel contains the images that will be deleted (array of image id)
+//files contains the new images that will be added (array of image) (checked at upload section)
+//productAction contains the action and quantity to update the product (object). Both will always be present (validation checked)
+//If action is add, will add the quantity to the product. If action is reduce, will reduce the quantity from the product
+//Old images will be deleted from cloudinary and from the product's images field
+//New images will be added to the product's images field
+//If no imagesDel is provided, no images will be deleted
+//If no files is provided, no images will be added
+//If no productAction is provided, no quantity will be changed
+//Quantity will not be modifiable outside of productAction
+//Flow : Get product -> Update information -> Update quantity -> Delete images -> Add images -> Save product
+//Note: Image update must be done in multipart form data.
+module.exports.updateProduct = async (req, res, next) => {
     try {
+        //Get product
         const { id } = req.params;
         const product = await Product.findById(id);
         const imagesDel = req.body.imagesDel;
         const updateFields = req.body;
         if (!product) {
-            return res.status(404).json({ error: 'Product not found', success: false });
+            return res.status(404).json({ error: 'Product not found' });
         }
-        //Update the product
+        //Update the product information part
         for (const [key, value] of Object.entries(updateFields)) {
+            //Exclude quantity
+            if (key === 'quantity') {
+                continue;
+            }
             product[key] = value;
         }
+        //Handle update quantity
+        if (updateFields.productAction) {
+            const { action, quantity } = updateFields.productAction;
+            //Parse quantity to int
+            const intQuantity = parseInt(quantity);
+            if (action === 'add') {
+                product.quantity += intQuantity;
+            }
+            else if (action === 'reduce') {
+                product.quantity = Math.max(product.quantity - intQuantity, 0);
+            }
+        }
+        else
+            console.log("No quantity or action provided. Quantity will not be changed.");
+
+        //If imagesDel is provided, delete the images
         if (imagesDel) {
             const imageIds = [...imagesDel]
             for (const imageId of imageIds) {
@@ -191,11 +239,10 @@ module.exports.updateProduct = async (req, res) => {
         res.json({ msg: 'Product updated', product: savedProduct, success: true });
 
     } catch (e) {
-        console.error(e);
-        res.status(500).json({ error: 'Internal Server Error', success: false });
+        next(e);
     }
 }
-//Delete product
+//Delete product: Have post function to call delete images from cloudinary
 module.exports.deleteProduct = async (req, res) => {
     try {
         const { id } = req.params;
