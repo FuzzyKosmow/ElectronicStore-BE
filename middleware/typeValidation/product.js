2880594920ad4e18817cd4fb80bd2ec454bf8ae0
Shoru <bapcui596@gmail.com>
2023-12-13 01:47:22 +0700

    improvement(product): adding type validation for action on quantity

--- middleware/typeValidation/product.js
+++ middleware/typeValidation/product.js
@@ -7,13 +7,17 @@ const CreateProductSchema = joi.object({
     manufacturer: joi.string().required().max(50),
     sellPrice: joi.number().min(0),
     importPrice: joi.number().min(0),
+    quantity: joi.number().min(0),
     countryOrigin: joi.string().required().max(50),
     images: joi.array().items(joi.object({
         url: joi.string(),
         filename: joi.string(),
     })),
 });
-
+//Update consist of action to update information if needed as well as image. 
+//Action can be add or reduce quantity of product. Default is add.
+//If no action is specified, it will be add.
+//If no quantity is specified, it will be 0.
 const UpdateProductSchema = joi.object({
     productName: joi.string().max(50),
     type: joi.string().max(50),
@@ -26,13 +30,17 @@ const UpdateProductSchema = joi.object({
         url: joi.string(),
         filename: joi.string(),
     })),
+    //Product action : add with X quantity or reduce with X quantity// Both must be present
+    productAction: joi.object({
+        action: joi.string().valid('add', 'reduce').required(),
+        quantity: joi.number().min(0).required(),
+    }).optional(),
     imagesDel: joi.array().items(joi.string()),
-});
-
+})
 const ValidateCreateProduct = (req, res, next) => {
     const { error } = CreateProductSchema.validate(req.body);
     if (error) {
-        return res.status(400).json({ error: error.details[0].message, success: false });
+        return res.status(400).json({ error: error.details[0].message });
     }
     next();
 }
@@ -40,7 +48,7 @@ const ValidateCreateProduct = (req, res, next) => {
 const ValidateUpdateProduct = (req, res, next) => {
     const { error } = UpdateProductSchema.validate(req.body);
     if (error) {
-        return res.status(400).json({ error: error.details[0].message, success: false });
+        return res.status(400).json({ error: "Schema validation error: " + error.details[0].message });
     }
     next();
 }
