9e9e35899c88bd17314c205228cfbb75cef738f9
Shoru <bapcui596@gmail.com>
2023-12-17 23:58:08 +0700

    improvement(order): order now reflect change on product quantity

--- controllers/orders.js
+++ controllers/orders.js
@@ -14,7 +14,7 @@ function isValidDate(dateString) {
     const regex = /^\d{2}\/\d{2}\/\d{4}$/;
     return regex.test(dateString);
 }
-async function  ConvertOrderQuery(query)  {
+async function ConvertOrderQuery(query) {
     const filter = {};
     if (query.customerName) {
         //Find customer whose name contains the query
@@ -28,8 +28,8 @@ async function  ConvertOrderQuery(query)  {
     //Date type check to ensure it's in DD/MM/YYYY format
     if (query.orderAfterDate || query.orderBeforeDate || query.orderDate) {
         if (!isValidDate(query.orderAfterDate) && query.orderAfterDate) {
-            throw new ExpressError('Invalid orderAfterDate format. Accepted format: DD/MM/YYYY', 400);      
-          }
+            throw new ExpressError('Invalid orderAfterDate format. Accepted format: DD/MM/YYYY', 400);
+        }
         if (!isValidDate(query.orderBeforeDate) && query.orderBeforeDate) {
             throw new ExpressError('Invalid orderBeforeDate format. Accepted format: DD/MM/YYYY', 400);
         }
@@ -59,7 +59,7 @@ async function  ConvertOrderQuery(query)  {
     return filter;
 }
 
-module.exports.getOrders = async (req, res,next) => {
+module.exports.getOrders = async (req, res, next) => {
     const limit = req.query.limit;
     const startIndex = req.query.startIndex;
     let filter = {};
@@ -67,11 +67,30 @@ module.exports.getOrders = async (req, res,next) => {
     try {
         filter = await ConvertOrderQuery(req.query);
         results.results = await Order.find(filter).limit(limit).skip(startIndex).exec();
-        res.status(200).json({ ...results, success: true });
+        res.status(200).json({ ...results });
     } catch (error) {
-       next(error);
+        next(error);
     }
 }
+
+//Get order detail 
+module.exports.getOrderDetail = async (req, res, next) => {
+    try {
+        const { id, detailId } = req.params;
+        const order = await Order.findById(id);
+        if (!order) {
+            return res.status(404).json({ error: 'Order not found' });
+        }
+        const orderDetail = await OrderDetail.findById(detailId);
+        if (!orderDetail) {
+            return res.status(404).json({ error: 'Order detail not found' });
+        }
+        res.status(200).json({ orderDetail, success: true });
+    } catch (error) {
+        next(error);
+    }
+}
+//if status is delivered, update product quantity
 module.exports.addOrder = async (req, res, next) => {
     const orderDetails = req.body.orderDetails;
     const order = new Order({
@@ -81,7 +100,30 @@ module.exports.addOrder = async (req, res, next) => {
         status: 'Pending',
         orderDetails: [],
         total: 0
-    });``
+    });
+
+    //Check for duplicate product id. If found, return error
+    const duplicateProduct = orderDetails.some((orderDetail, index) => {
+        return orderDetails.some((orderDetail2, index2) => {
+            return index !== index2 && orderDetail.productId === orderDetail2.productId;
+        });
+    });
+    if (duplicateProduct) {
+        return res.status(400).json({ error: 'Duplicate product id' });
+    }
+
+    //Check if customer exists. Cusoemr id can be null
+    const customer = await Customer.findById(order.customerId);
+    if (!customer && order.customerId !== null) {
+        return res.status(400).json({ error: 'Customer not found' });
+    }
+    //Check if employee exists. 
+    const employee = await Employee.findById(order.employeeId);
+    if (!employee) {
+        return res.status(400).json({ error: 'Employee not found' });
+    }
+
+
     try {
         //Save order details
         for (const orderDetail of orderDetails) {
@@ -89,17 +131,25 @@ module.exports.addOrder = async (req, res, next) => {
             if (!product) {
                 return res.status(400).json({ error: 'Product not found.' });
             }
+            //Check if quantity is valid
+            if (orderDetail.quantity > product.quantity) {
+                return res.status(400).json({ error: 'Invalid quantity. Not enough quantity for product ' + product.productName });
+            }
             const newOrderDetail = new OrderDetail({
                 productId: orderDetail.productId,
-                quantity: orderDetail.quantity,
-                sellPrice: product.sellPrice,
+                //Parse to int
+                quantity: parseInt(orderDetail.quantity),
+                //Parse to double
+                sellPrice: parseFloat(product.sellPrice),
                 orderId: order._id
             });
+
+            await product.save();
             await newOrderDetail.save();
             order.orderDetails.push(newOrderDetail._id);
         }
         await order.save();
-        console.log("saved order");
+
         res.status(200).json({ msg: 'Order added', order: order });
     } catch (error) {
         next(error);
@@ -124,18 +174,26 @@ module.exports.getOrder = async (req, res, next) => {
     }
 }
 //Generally used to udpate status of order. Can also be used to update customer id, employee id , order date and order details.
-module.exports.updateOrder = async (req, res) => {
+//If status is updated to delivered, update product quantity
+//If order is already delivered, cannot be updated
+module.exports.updateOrder = async (req, res, next) => {
 
     try {
         const { id } = req.params;
+        if (!id || id === undefined) {
+            return res.status(400).json({ error: 'Order id not provided' });
+        }
         const order = await Order.findById(id);
         if (!order) {
             return res.status(404).json({ error: 'Order not found' });
         }
-
+        if (order.status === 'Delivered') {
+            return res.status(400).json({ error: 'Order is already delivered. Cannot be updated' });
+        }
+        //Go through each update value and update it
         for (const key in req.body) {
             //If order details are updated, ignore it. Handle it separately
-            if (key === 'orderDetails' || key === 'deleteOrderDetails') {
+            if (key === 'newOrderDetails' || key === 'deleteOrderDetails') {
                 continue;
             }
             //Handle date conversion (DD/MM/YYYY)
@@ -147,6 +205,31 @@ module.exports.updateOrder = async (req, res) => {
                     //Remove time zone offset
                     req.body[key] = new Date(req.body[key].getTime() - req.body[key].getTimezoneOffset() * 60 * 1000);
                 }
+            }
+            //Handle status update. Ignore, handle it to update product quantity later
+            if (key === 'status') {
+                continue;
+            }
+            //Handle customer id. If customer id is invalid and not null, throw error
+
+            if (key === 'customerId') {
+                const customer = Customer.findById(req.body[key]);
+                if (!customer && req.body[key] !== null) {
+                    return res.status(400).json({ error: 'Invalid customer id' });
+                }
+                else if (req.body[key] === null) {
+                    req.body[key] = undefined;
+                }
+                continue;
+
+            }
+            //Handle employee id. If employee id is invalid, throw error
+            if (key === 'employeeId') {
+                const employee = Employee.findById(req.body[key]);
+                if (!employee) {
+                    return res.status(400).json({ error: 'Invalid employee id' });
+                }
+                continue;
             }
             order[key] = req.body[key];
         }
@@ -165,10 +248,11 @@ module.exports.updateOrder = async (req, res) => {
 
         //Handle order details to add
         if (req.body.newOrderDetails) {
+
             const newOrderDetails = [...req.body.newOrderDetails];
             for (const orderDetail of newOrderDetails) {
                 //Check if product exists - > if not, ignore it. If it does, add it to order details
-                const product = mongoose.model('Product').findById(orderDetail.productId);
+                const product = await Product.findById(orderDetail.productId);
                 if (!product) {
                     continue;
                 }
@@ -178,9 +262,48 @@ module.exports.updateOrder = async (req, res) => {
                     sellPrice: product.sellPrice,
                     orderId: order._id
                 });
+                //Check if quantity is valid
+                if (orderDetail.quantity > product.quantity) {
+                    return res.status(400).json({ error: 'Invalid quantity. Not enough quantity for product ' + product.productName });
+                }
+
                 await newOrderDetail.save();
                 order.orderDetails.push(newOrderDetail._id);
             }
+        }
+        //Handle status update and product quantity update
+        if (req.body.status) {
+            if (order.status === req.body.status) {
+                //Ignore duplicate status
+                console.log("Duplicate status");
+            }
+
+            //If status is delivered, update product quantity
+            //Go through order details and update product quantity in current order
+            else if (req.body.status === 'Delivered') {
+                const orderDetails = [...order.orderDetails];
+                for (const orderDetailId of orderDetails) {
+                    const orderDetail = await OrderDetail.findById(orderDetailId);
+                    const product = await Product.findById(orderDetail.productId);
+
+                    //If product is not found, throw error
+                    if (!product) {
+                        console.log("Product with id: ", orderDetailId.productId, " not found");
+                        return res.status(400).json({ error: 'Product not found' });
+                    }
+                    //If dont have enough quantity, throw error
+                    if (product.quantity < orderDetailId.quantity) {
+                        return res.status(400).json({ error: 'Not enough quantity for product ' + product.name });
+                    }
+
+                    product.quantity = Math.max(0, parseInt(product.quantity) - parseInt(orderDetail.quantity));
+                    await product.save();
+
+                }
+                order.status = req.body.status;
+                console.log("Status updated");
+            }
+
         }
         await order.save();
         res.status(200).json({ msg: 'Order updated', order: order });
