d809f0be19f9f66daffd5c0745db38f73f045de1
Shoru <bapcui596@gmail.com>
2023-12-18 00:00:55 +0700

    fix(order): handling invalid quantity

--- middleware/typeValidation/order.js
+++ middleware/typeValidation/order.js
@@ -1,30 +1,37 @@
 const joi = require('joi').extend(require('@joi/date'));
+joi.objectId = require('joi-objectid')(joi);
+
 //Receive customer id, employee id and order details only.  Order date will be set to current date and status will be set to 'pending'
 const CreateOrderSchema = joi.object({
-    customerId: joi.string().required(),
-    employeeId: joi.string().required(),
+    customerId: joi.objectId().required(),
+    employeeId: joi.objectId().required(),
     orderDetails: joi.array().items(joi.object({
-        productId: joi.string().required(),
+        productId: joi.objectId().required(),
         quantity: joi.number().required(),
     })).required(),
 });
 // Can receive customer id, employee id, order date and status. Order details will be ignored
 const UpdateOrderSchema = joi.object({
-    customerId: joi.string(),
-    employeeId: joi.string(),
+    customerId: joi.objectId(),
+    employeeId: joi.objectId(),
     //DD/MM/YYYY
     orderDate: joi.date().format('DD/MM/YYYY'),
     status: joi.string().valid('Pending', 'In Progress', 'Delivered'),
     newOrderDetails: joi.array().items(joi.object({
-        productId: joi.string().required(),
-        quantity: joi.number().required(),
+        productId: joi.objectId().required(),
+        quantity: joi.number().min(0).required(),
     })),
-    deleteOrderDetails: joi.array().items(joi.string())
+    deleteOrderDetails: joi.array().items(joi.object({
+        productId: joi.objectId().required(),
+    })),
+
 });
 
 const ValidateCreateOrder = (req, res, next) => {
     const { error } = CreateOrderSchema.validate(req.body);
     if (error) {
+
+        console.log("Create order validation error: ", error);
         return res.status(400).json({ error: error.details[0].message });
     }
     next();
