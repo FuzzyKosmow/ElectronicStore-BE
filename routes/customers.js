262382889cf89bca7e7da250f0db1f87e096bd7a
Shoru <bapcui596@gmail.com>
2023-12-18 22:39:35 +0700

    feat(customer): additional pagination and id checking

--- routes/customers.js
+++ routes/customers.js
@@ -3,14 +3,20 @@ const router = express.Router();
 const { authorize } = require('../middleware/auth');
 const customerController = require('../controllers/customers');
 const { ValidateCreateCustomer, ValidateUpdateCustomer } = require('../middleware/typeValidation/customer');
-
+const IsValidObjectId = require('../middleware/typeValidation/validObjectId');
+const ValidatePagination = require('../middleware/validatePageLimit');
+const Customer = require('../models/customer');
+const ValidatePageWrapper = async (req, res, next) => {
+    ValidatePagination(req, res, next, () => Customer.countDocuments());
+}
 router.route('/')
-    .get(authorize(['employee', 'admin']), customerController.getCustomers)
+    .get(authorize(['employee', 'admin']), ValidatePageWrapper, customerController.getCustomers)
     .post(authorize(['employee', 'admin']), ValidateCreateCustomer, customerController.addCustomer);
 
 router.route('/:id')
-    .get(authorize(['employee', 'admin']), customerController.getCustomer)
-    .patch(authorize(['employee', 'admin']), ValidateUpdateCustomer, customerController.updateCustomer)
-    .delete(authorize(['employee', 'admin']), customerController.deleteCustomer);
+    .get(authorize(['employee', 'admin']), IsValidObjectId, customerController.getCustomer)
+    .patch(authorize(['employee', 'admin']), IsValidObjectId, ValidateUpdateCustomer, customerController.updateCustomer)
+    .delete(authorize(['employee', 'admin']), IsValidObjectId, customerController.deleteCustomer);
+
 
 module.exports = router;
