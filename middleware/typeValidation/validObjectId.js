57d39ba8889456354826b60ecc671e8a1b87eac4
Shoru <bapcui596@gmail.com>
2023-12-18 22:38:07 +0700

    chore: adding more logging

--- middleware/typeValidation/validObjectId.js
+++ middleware/typeValidation/validObjectId.js
@@ -2,10 +2,14 @@ const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 function IsValidObjectId(req, res, next) {
   const { error } = Joi.objectId().validate(req.params.id);
-  if (error) return res.status(400).json({ 
-    error: error.details[0].message, 
-    success: false
-});
+
+  if (error) {
+    console.log("Invalid object id. Made to:" + req.method + " " + req.originalUrl);
+    return res.status(400).json({
+      error: error.details[0].message,
+      success: false
+    });
+  }
   next();
 }
 
