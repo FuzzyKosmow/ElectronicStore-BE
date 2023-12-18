1a8be7c6853082f84fa8a3683dccbabd768b4ad1
Shoru <bapcui596@gmail.com>
2023-12-18 22:40:54 +0700

    feat(user): added authorization

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,9 @@ const router = express.Router();
 const passport = require('passport');
 const usersController = require('../controllers/users');
 const { getCurrentUser } = require('../middleware/auth');
-
+const { authorize } = require('../middleware/auth');
 router.route('/register')
-    .post(usersController.registerEmployee);
+    .post(authorize(['admin']), usersController.registerEmployee);
 router.route('/login')
     .get(getCurrentUser)
     .post(usersController.login);
