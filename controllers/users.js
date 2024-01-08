3ae42b7c9883e1d30cadafa2e98e497470db291e
Shoru <bapcui596@gmail.com>
2024-01-08 15:08:41 +0700

    feat(user): change password

--- controllers/users.js
+++ controllers/users.js
@@ -31,6 +31,33 @@ module.exports.registerEmployee = async (req, res, next) => {
         next(e);
     }
 }
+module.exports.changePassword = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { password } = req.body;
+        //Find the employee
+
+        const employee = await Employee.findById(id);
+        //Check if the employee exists
+        if (!employee) {
+            return res.status(404).json({ error: 'Employee not found' });
+        }
+        //Find associated user
+        const user = await User.findOne({ employeeId: id });
+        //Check if the user exists
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        //Change password
+        await user.setPassword(password);
+        await user.save();
+        res.status(200).json({ msg: 'Password changed' });
+    }
+    catch (e) {
+        next(e);
+    }
+}
 module.exports.registerAdmin = async (req, res, next) => {
     try {
         console.log('registerAdmin');
@@ -70,14 +97,29 @@ module.exports.login = (req, res) => {
             if (!user) {
                 return res.status(400).json({ error: 'Invalid username or password' });
             }
-            req.logIn(user, function (err) {
+            req.logIn(user, async function (err) {
                 if (err) {
                     console.log(err);
                     return next(err);
                 }
+
+                const employee = await Employee.findById(user.employeeId);
+                if (employee) {
+                    user.position = employee.position;
+                }
+
                 //Set user
                 res.user = user;
-                res.status(200).json({ msg: 'Login success', employeeId: user.employeeId, username: user.username, role: user.role });
+
+                //Find associated employee and get position
+
+                res.status(200).json({
+                    msg: 'Login success',
+                    employeeId: user.employeeId,
+                    username: user.username,
+                    role: user.role,
+                    position: user.position
+                });
             });
         })(req, res);
     }
